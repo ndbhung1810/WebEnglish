9cec65b17edbc42b87ebf16ea8aceaac4b0b3126
ndbhung1810 <nguyendbhung1810@gmail.com>
2024-01-01 20:51:42 +0700

    fix bug

--- validatons/auth.validation.js
+++ validatons/auth.validation.js
@@ -14,7 +14,7 @@ module.exports = {
         password: Joi.string().min(8).max(16).required().external(validatePassword),
         roleid:Joi.number().required(),
         birthday:Joi.string(),
-        image:Joi.string(),
+        image: Joi.string().allow("", null),
     }),
     login: Joi.object().keys({
         email: Joi.string().email().required(),
